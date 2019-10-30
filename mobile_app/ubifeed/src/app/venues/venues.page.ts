6045a4c162329b38ba8ccb756f99a7646be1b227
MarcoWilhelm <marcowilhelm@gmx.at>
2019-10-30 16:40:50 +0000

    started with venues screen

--- mobile_app/ubifeed/src/app/venues/venues.page.ts
+++ mobile_app/ubifeed/src/app/venues/venues.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VenuesPage implements OnInit {
 
+  venues : any;
+
   constructor() { }
 
   ngOnInit() {
+    fetch('../../assets/data/venues.json').then(res => res.json())
+    .then(json => {
+      this.venues = json;
+    });
   }
 
 }
